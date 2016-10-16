b609b42b60429a6684a3567878fa54c5401b6087
ojflores <ojflores@puc.edu>
2016-10-16 22:43:12 +0000

    comments added

--- lab3.c
+++ lab3.c
@@ -5,6 +5,8 @@
 #define MIN -20000
 
 void max_heapify(int arr[], int i, int counter){
+	//uses a counter to know how big the array is
+	//l and r represent left and right node respectively
 	int l = 2 * i;	//this is l as in lamb, not the number
 	int r = (2 * i) + 1;
 	int largest, temp;
@@ -17,10 +19,12 @@ void max_heapify(int arr[], int i, int counter){
 	if (r <= counter && arr[r] > arr[largest]){
 		largest = r;
 	}
+	
 	if (largest != i){
 		temp = arr[i];
 		arr[i] = arr[largest];
 		arr[largest] = temp;
+		//switches to put the largest at the bottom
 		max_heapify(arr, largest, counter);
 	}
 }
@@ -28,12 +32,14 @@ void max_heapify(int arr[], int i, int counter){
 void build_max_heap(int arr[], int counter){
 	int heap_size = counter;
 	int i;
+	//
 	for (i = heap_size/2; i >= 0; i--){
 		max_heapify(arr, i, counter);
 	}
 }
 
 void heap_sort(int arr[], int counter){
+	//initialization
 	build_max_heap(arr, counter);
 	int i, temp;
 	for (i = counter; i >= 0; i--){
@@ -42,8 +48,9 @@ void heap_sort(int arr[], int counter){
 		arr[i] = temp;
 		counter -= 1;
 		max_heapify(arr, 0, counter);
-		
+		//maintenance
 	}
+	//termination
 }
 
 int heap_extract_max(int arr[], int heap_size){
@@ -82,6 +89,7 @@ void max_heap_insert(int arr[], int key, int heap_size){
 
 void print_array(int arr[], int counter){
 	int c;
+	counter += 1;
 	for (c = 0; c < counter; c++){
 		printf("%d\n", arr[c]);
 	}
@@ -103,6 +111,7 @@ void main(){
 	//sets the counter up correctly
 	counter -= 1;
 	
+	//seperated for readability
 	heap_sort(a, counter);
 	print_array(a, counter);
 	printf("\n");
@@ -117,5 +126,3 @@ void main(){
 	print_array(a, counter);
 	fclose(f);
 }
-
-
