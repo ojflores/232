1639b81bbb18734f4d224c81386399c4f08154eb
ojflores <ojflores@puc.edu>
2016-10-20 22:31:26 +0000

    j for loop fixed

--- lab4.c
+++ lab4.c
@@ -8,9 +8,11 @@ void counting_sort(int A[], int B[], int k, int length){
 	for (i = 0; i <= k; i++){
 		C[i] = 0;
 	}
-	for (j = 1; j < length; j++){
+	for (j = 0; j <= length; j++){
 		C[A[j]] = C[A[j]] + 1;
 	}
+	//for (i = 1; i < k; i++){}
+		
 	
 	
 	
@@ -23,10 +25,10 @@ void counting_sort(int A[], int B[], int k, int length){
 }
 
 void main(){
-	int A[20] = {2, 4, 3, 1};
+	int A[20] = {2, 5, 3, 0, 2, 3, 0, 3};
 	int B[20];
-	int k = 4;
-	int length = 4;
+	int k = 5;
+	int length = 7;
 	counting_sort(A, B, k, length);
 	
 }
