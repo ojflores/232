6a2b3b0792fc6ca37abee8a9e4727c84f9d7d078
ojflores <ojflores@puc.edu>
2016-10-21 00:08:27 +0000

    one indexed

--- lab4.c
+++ lab4.c
@@ -1,35 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define AMOUNT_OF_NUMBERS 10
+#define MAX 100000
+
 
 
 void counting_sort(int A[], int B[], int k, int length){
-	int C[k];
+	int C[k + 1];
 	int i, j;
 	for (i = 0; i <= k; i++){
 		C[i] = 0;
 	}
-	for (j = 0; j <= length; j++){
+	for (j = 1; j <= length; j++){
 		C[A[j]] = C[A[j]] + 1;
 	}
 	for (i = 1; i <= k; i++){
 		C[i] = C[i] + C[i - 1];
 	}
-	for (j = 7; j >= 0; j--){
+	for (j = length; j >= 1; j--){
 		B[C[A[j]]] = A[j];
 		C[A[j]] = C[A[j]] - 1;
 	}
-	
+	//return B;
+}
+
+void print_array(int arr[], int amount){
 	int d;
-	for (d = 1; d <= 8; d++){
-		printf("%d\n", B[d]);
+	for (d = 1; d <= amount; d++){
+		printf("%d\n", arr[d]);
 	}
 }
 
 void main(){
-	int A[20] = {2, 5, 3, 0, 2, 3, 0, 3};
+	int A[20] = {0, 2, 5, 3, 0, 2, 3, 0, 3};
 	int B[20];
 	int k = 5;
-	int length = 7;
-	counting_sort(A, B, k, length);
+	int length = 8;
+	//counting_sort(A, B, k, length);
+	//print_array(B, 8);
+	
+	int i;
+	int array[AMOUNT_OF_NUMBERS+1];
+	int arraybn[AMOUNT_OF_NUMBERS+1];
+	for (i = 1; i <	 1+AMOUNT_OF_NUMBERS; i++){
+		array[i] = rand() % 10;
+	}
+	//counting_sort(array, arraybn, 15, AMOUNT_OF_NUMBERS);
+	counting_sort(A, arraybn, 15, 8);
+	//print_array(array, AMOUNT_OF_NUMBERS);
+	print_array(A, 8);
+	printf("\n");
+	//print_array(arraybn, AMOUNT_OF_NUMBERS);
+	print_array(arraybn, 8);
+	
+	
+	
+	
 	
 }
