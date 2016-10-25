5d7f3f71dfd43e329da067f118d47ef5f651ced4
ojflores <ojflores@puc.edu>
2016-10-25 02:23:18 +0000

    r fixed

--- partition.c
+++ partition.c
@@ -4,12 +4,33 @@
 #define INPUT "input.txt"
 
 
-void quicksort(){
-	
+
+
+int partition(int A[], int p, int r){
+	int x = A[r];
+	int i = p - 1;
+	int temp, j;
+	for (j = p; j <= r - 1; j++){
+		if (A[j] <= x){
+			i++;
+			temp = A[i];
+			A[i] = A[j];
+			A[j] = temp;
+		}
+	}
+	temp = A[i + 1];
+	A[i + 1] = A[r];
+	A[r] = temp;
+	return i + 1;
 }
 
-void partition(){
-	
+void quicksort(int A[], int p, int r){
+	int q;
+	if (p < r){
+		q = partition(A, p, r);
+		quicksort(A, p, q - 1);
+		quicksort(A, q + 1, r);
+	}
 }
 
 void print_array(int arr[], int counter){
@@ -37,7 +58,10 @@ void main(){
 	counter -= 1;
 	//counter = r
 	print_array(a, counter);
+	printf("\n");
 	
+	quicksort(a, 0, counter);
+	print_array(a, counter);
 	
 	
 }
