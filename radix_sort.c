d77ac5db6b3d28207d2324b9df4cbe548f8ae64a
ojflores <ojflores@puc.edu>
2016-11-01 00:39:03 +0000

    copied a[j] to a[b]

---
+++ radix_sort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define AMOUNT_OF_NUMBERS 10
+//#define MAX 100000
+
+void print_array(int arr[], int amount){
+	int c;
+	amount += 1;
+	//starts at 1 because of indexing isssue
+	for (c = 1; c <= amount; c++){
+		printf("%d\n", arr[c]);
+	}
+}
+
+void counting_sort(int A[], int B[], int k, int length, int o){
+	int C[k];
+	int i, j, mod = 10;
+	
+	//makes everything in C zero
+	for (i = 0; i <= k; i++){
+		C[i] = 0;
+	}
+	
+	//adds a counter so that we know how many there are
+	for (j = 0; j <= length; j++){
+		C[(A[j] / o) % mod] = C[(A[j] / o) % mod] + 1;
+	}
+	
+	//adds the numbers previous to make so that things will go into the correct array in the next for loop
+	for (i = 1; i < (k+1); i++){
+		C[i] = C[i] + C[i-1];
+	}
+	//B here is 1 indexed, different than C and A which are 0 indexed
+	for (j = length; j >= 0; j--){
+		B[C[(A[j] / o) % mod]] = A[j];
+		C[(A[j] / o) % mod] = C[(A[j] / o) % mod] - 1;
+	}
+	for (j = 0; j <= length; j++){
+		A[j] = B[j+1];
+	}
+	
+}
+
+
+void main(){
+	int A[20] = {24, 50, 32, 12, 22, 33, 10, 33, 28};
+	int B[20];
+	int k = 8;
+	int length = 8;
+	
+	
+	int i, end = 0, o = 1;
+	for (i = 2; i > end; i--){
+		counting_sort(A, B, k, length, o);
+		print_array(B, length);
+		printf("\n");
+		o = o * 10;
+		
+	}
+}
+
