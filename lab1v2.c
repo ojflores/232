90c6b21b18ac2d204b4054b52446a11111bba64a
ojflores <ojflores@puc.edu>
2016-11-10 06:54:25 +0000

    comments added/looks better

--- lab1v2.c
+++ lab1v2.c
@@ -6,20 +6,19 @@
 
 void insertion_sort(int a[], int counter){
 	int j, i, key;			
-	//initialization
+	//initialization: j == 1, A[] only holds one sorted item in A[0]
 	//"j" starts at one as opposed to 0 because the first number is already sorted when by itself
 	for (j = 1; j < counter; j++){
 		key = a[j];
 		i = j - 1;
-		//
 		while (i >= 0 && a[i] > key){
 			a[i+1] = a[i];
 			i -= 1;
 		}
 		a[i+1] = key;
-		//maintenance
+		//maintenance: for loop moves by A[j-1] until correct place is found
 	}
-	//termination
+	//termination: j>A.length and everything is in its rightful place
 }
 
 int main()
@@ -45,26 +44,8 @@ int main()
 		printf("%d\n", a[c]);
 	}
 	
-	printf("now the timed test: \n");
-	
-	//creating a list of random to time how long it takes for it to run
-	int array[MAX];
-	n = MAX;
-	for (i = 0; i < n; i++){
-		array[i] = rand() % MAX;
-	}
-	
-	
-	clock_t begin = clock();
-	
-	insertion_sort(array, n);
-	
-	clock_t end = clock();
-	double time_spent = (double)(end - begin)/CLOCKS_PER_SEC;
-	printf("time spent: %f\n", time_spent);
-	
-	
-	
 	fclose(f);	
 	return 0;
 }
+
+
