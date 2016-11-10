af7e058a0899287dcbbe33fa69689096eb1d7c51
ojflores <ojflores@puc.edu>
2016-11-10 07:09:29 +0000

    loop invarients added

--- lab2.c
+++ lab2.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX 200
+#define MAX 100000
+#define SPLIT 600000
 
 merge(int arr[], int p, int q, int r){
 	int n1 = q - p + 1;
 	int n2 = r - q;
-	int L[15];
-	int R[15];
+	int L[SPLIT];
+	int R[SPLIT];
 	int i;
 	int j;
 	for(i = 1; i <= n1; i++){
@@ -16,11 +17,12 @@ merge(int arr[], int p, int q, int r){
 	for(j = 1; j <= n2; j++){
 		R[j] = arr[q + j];
 	}
-	L[n1 + 1] = MAX;
-	R[n2 + 1] = MAX;
+	L[n1 + 1] = MAX + 1;
+	R[n2 + 1] = MAX + 1;
 	i = 1;
 	j = 1;
 	int k;
+	//initialization: k = p, A[p..k-1] is empty, L[i] and R[i] are the smallest elements in the array and have not been copied into A
 	for(k = p; k <= r; k++){
 		if(L[i] <= R[j]){
 			arr[k] = L[i];
@@ -31,6 +33,7 @@ merge(int arr[], int p, int q, int r){
 			j +=1;
 		}
 	}
+	//maintenance: A[p..k] contain k-p+1 smallest elements
 	
 }
 
@@ -40,12 +43,14 @@ merge(int arr[], int p, int q, int r){
 
 print_array(int arr[], int counter){	//test to see if array can be passed
 	int c;
+	counter += 1;
 	for (c = 0; c < counter; c++){
 		printf("%d\n", arr[c]);
 	}
 }
 
 merge_sort(int arr[], int p, int r){
+	
 	int q;
 	if(p < r) {
 		q = (p + r)/2;
@@ -53,9 +58,11 @@ merge_sort(int arr[], int p, int r){
 		merge_sort(arr, q + 1, r);
 		merge(arr, p, q, r);
 	}
+	//termination: things are now in sorted order and copied into A
 }
 
-main(){
+
+int main(){
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
@@ -70,17 +77,14 @@ main(){
 		a[counter] = atoi(line);
 		counter += 1;
 	}
-
-	clock_t begin = clock();
+	counter -= 1;
 	
-	merge_sort(a, 0, counter - 1);
-	clock_t end = clock();
-	double time_spent = (double)(end - begin)/CLOCKS_PER_SEC;
-	printf("time spent: %f\n", time_spent);
+	
+	merge_sort(a, 0, counter);
+	
 	
 	print_array(a, counter);
-	
-	
+	printf("\n");
+	return 0;
 }
-//doesn't work above the number 200
-//turns out lab1 runs a little bit faster
+
